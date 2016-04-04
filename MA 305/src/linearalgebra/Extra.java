de7d9e55de44087875d83148aa88e77c000cf28e
jdmalter <jdmalter@ncsu.edu>
2016-04-04 14:03:13 -0400

    Added new overconstrained function in Extra which works with singular value decompositon and added test for new function in extraTest. Fixed naming issue where overconstrained was spelled overconstrainted.

--- MA 305/src/linearalgebra/Extra.java
+++ MA 305/src/linearalgebra/Extra.java
@@ -1035,4 +1035,420 @@ public class Extra {
 		return new double[][][] { x, e };
 	}
 
+	/**
+	 * Assumes row by columnn array representation of matrix. Given that x is
+	 * the first element of the resulting array and e is the second element of
+	 * the resulting array, a*x (through matrix multiplication) - b equals e
+	 * where the magnitude of e is minimized. Returns array of array of n by 1
+	 * and m by 1 array of double,
+	 * 
+	 * This implementation finds x in one step. Given that array usv is the
+	 * resulting array of sv decomposition on given matrix a where ut is
+	 * tranpose of the first element of usv, (s^-1) is the inverse of the second
+	 * element of usv, v is the third element of usv, x is equal to
+	 * v*(s^-1)*ut*b (through multiple matrix multiplications).
+	 * 
+	 * Throws IllegalArgumentException if any of the following is true:
+	 * {@code (null == a)}, {@code (null == b)}, {@code (a.length != b.length)}
+	 * where length is number of rows, {@code (null == a[row])} where a[row] is
+	 * any row, {@code (null == b[row])} where b[row] is any row,
+	 * {@code (a.length <= acol)} where length is number rows and acol is number
+	 * of columns on first row, {@code (acol != a[row].length)} where acol is
+	 * number of columns on first row and a[row].length is number of columns, or
+	 * {@code (1 != b[row].length)} where b[row].length is number of columns on
+	 * any row.
+	 * 
+	 * There is a special condition where divide by 0 occurs when the
+	 * determinant of the resulting matrix from at*a (through matrix
+	 * multiplication) eqauls 0.
+	 * 
+	 * @param a
+	 *            m by n array of double where m > n
+	 * @param b
+	 *            m by 1 array of double
+	 * @return array of n by 1 and m by 1 array of double
+	 */
+	public static double[][][] overconstrainedSV(double[][] a, double[][] b) {
+		if (null == a)
+			throw new IllegalArgumentException("matrix a must not be null");
+
+		else if (null == b)
+			throw new IllegalArgumentException("matrix b must not be null");
+
+		else if (a.length != b.length)
+			throw new IllegalArgumentException(
+					"matrix a and b number of rows must be equal");
+
+		int acol = -1;
+
+		for (int row = 0; row < a.length; row++)
+			if (null == a[row])
+				throw new IllegalArgumentException(
+						"matrix a must not contain null column");
+
+			else if (null == b[row])
+				throw new IllegalArgumentException(
+						"matrix b must not contain null column");
+
+			else if (row == 0) {
+				acol = a[0].length;
+				if (a.length <= acol)
+					throw new IllegalArgumentException(
+							"matrix a must contain more rows than columns");
+
+			} else if (acol != a[row].length)
+				throw new IllegalArgumentException(
+						"matrix a row width must remain constant");
+
+			else if (1 != b[row].length)
+				throw new IllegalArgumentException(
+						"matrix b must only contain one column");
+
+		// inline matrix multiplication and tranpose
+
+		double[][] ata = new double[acol][acol];
+
+		for (int row = 0; row < ata.length; row++) {
+			for (int col = 0; col < ata[row].length; col++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < a.length; brow++)
+					sum += a[brow][row] * a[brow][col];
+
+				ata[row][col] = sum;
+			}
+		}
+
+		// inline eigen
+
+		double[][] l = new double[ata.length][ata.length];
+		double[][] v = new double[ata.length][ata.length];
+
+		if (ata.length == 1) {
+
+			// assumed double a = -1;
+			double bb = ata[0][0];
+
+			l[0][0] = bb;
+
+			v[1][0] = 1;
+
+		} else if (ata.length == 2) {
+
+			// assumed double a = 1;
+			double bb = -ata[0][0] - ata[1][1];
+			double c = ata[0][0] * ata[1][1] - ata[0][1] * ata[1][0];
+			double sqrt = Math.sqrt((bb * bb) - (4 * c));
+
+			l[0][0] = (-bb + sqrt) / 2;
+			l[1][1] = (-bb - sqrt) / 2;
+
+			for (int row = 0; row < ata.length; row++) {
+				v[0][row] = -ata[0][1] / (ata[0][0] - l[row][row]);
+				v[1][row] = 1;
+			}
+
+			for (int row = 0; row < ata.length; row++) {
+				double mag = Math.sqrt((v[0][row] * v[0][row]) + 1);
+				v[0][row] /= mag;
+				v[1][row] /= mag;
+			}
+
+		} else if (ata.length == 3) {
+
+			// assumed double a = -1;
+			double bb = ata[0][0] + ata[1][1] + ata[2][2];
+			double c = -(ata[0][0] * ata[1][1]) - (ata[0][0] * ata[2][2])
+					- (ata[1][1] * ata[2][2]) + (ata[0][1] * ata[1][0])
+					+ (ata[0][2] * ata[2][0]) + (ata[1][2] * ata[2][1]);
+			double d = (ata[2][0] * ata[0][1] * ata[1][2])
+					+ (ata[1][0] * ata[2][1] * ata[0][2])
+					- (ata[0][0] * ata[2][1] * ata[1][2])
+					- (ata[1][1] * ata[2][0] * ata[0][2])
+					- (ata[2][2] * ata[1][0] * ata[0][1])
+					+ (ata[0][0] * ata[1][1] * ata[2][2]);
+
+			double f = (-(3 * c) - (bb * bb)) / 3;
+			double g = (-(2 * bb * bb * bb) - (9 * bb * c) - (27 * d)) / 27;
+			double h = (g * g / 4) + (f * f * f / 27);
+			double i = Math.sqrt((g * g / 4) - h);
+			double j = Math.pow(i, 1d / 3);
+			double k = Math.acos(-g / (2 * i));
+
+			double m = Math.cos(k / 3);
+			double n = Math.sqrt(3) * Math.sin(k / 3);
+			double p = (bb / 3);
+			double q = 2 * j * Math.cos(k / 3) + (bb / 3);
+			double r = -j * (m + n) + p;
+			double s = -j * (m - n) + p;
+
+			// more than 19 variables
+
+			if (q > r && q > s) {
+
+				// q is greatest root
+
+				l[0][0] = q;
+
+				if (r > s) {
+
+					// s is least root
+
+					l[1][1] = r;
+					l[2][2] = s;
+
+				} else {
+
+					// r is least root
+
+					l[1][1] = s;
+					l[2][2] = r;
+				}
+
+			} else if (r > s) {
+
+				// r is greatest root
+
+				l[0][0] = r;
+
+				if (q > s) {
+
+					// s is least root
+
+					l[1][1] = q;
+					l[2][2] = s;
+
+				} else {
+
+					// q is least root
+
+					l[1][1] = s;
+					l[2][2] = q;
+				}
+
+			} else {
+
+				// s is greatest root
+
+				l[0][0] = s;
+
+				if (q > r) {
+
+					// r is least root
+
+					l[1][1] = q;
+					l[2][2] = r;
+
+				} else {
+
+					// q is least root
+
+					l[1][1] = r;
+					l[2][2] = q;
+				}
+			}
+
+			for (int row = 0; row < ata.length; row++) {
+
+				// copy array sig into array x
+
+				double[][] x = new double[ata.length - 1][ata.length];
+				for (int xrow = 0; xrow < x.length; xrow++)
+					for (int xcol = 0; xcol < ata.length; xcol++) {
+						x[xrow][xcol] = ata[xrow][xcol];
+						if (xrow == xcol)
+							// if on the diagonal, then subtract lambda
+							x[xrow][xcol] -= l[row][row];
+					}
+
+				// inline forward elimination
+
+				for (int diag = 0; diag < x.length - 1; diag++) {
+					if (x[diag][diag] == 0)
+						throw new ArithmeticException("divide by 0");
+
+					for (int xrow = diag + 1; xrow < x.length; xrow++) {
+						double quotient = x[xrow][diag] / x[diag][diag];
+
+						for (int xcol = 0; xcol < x[xrow].length; xcol++)
+							x[xrow][xcol] -= (quotient * x[diag][xcol]);
+					}
+				}
+
+				// inline backward elimination
+
+				for (int diag = x.length - 1; diag > 0; diag--) {
+					if (x[diag][diag] == 0)
+						throw new ArithmeticException("divide by 0");
+
+					for (int xrow = diag - 1; xrow >= 0; xrow--) {
+						double quotient = x[xrow][diag] / x[diag][diag];
+
+						for (int xcol = 0; xcol < x[xrow].length; xcol++)
+							x[xrow][xcol] -= (quotient * x[diag][xcol]);
+					}
+				}
+
+				// solve for "z" where z is on row 5
+
+				v[0][row] = -x[0][2] / x[0][0];
+				v[1][row] = -x[1][2] / x[1][1];
+				v[2][row] = 1;
+			}
+
+			// normalize by row
+
+			for (int row = 0; row < ata.length; row++) {
+
+				// determinant on vector
+
+				double det = Math.sqrt((v[0][row] * v[0][row])
+						+ (v[1][row] * v[1][row]) + 1);
+				v[0][row] /= det;
+				v[1][row] /= det;
+				v[2][row] /= det;
+			}
+
+		} else
+			// because finding zeros of larger polynomials is difficult
+			throw new UnsupportedOperationException(
+					"Cannot handle matrices larger than 3 by 3");
+
+		double[][] inverse = new double[ata.length][ata.length];
+
+		for (int row = 0; row < ata.length; row++)
+			for (int col = 0; col < ata.length; col++) {
+				if (l[row][col] == 0)
+					// prevent divides by 0
+					inverse[row][col] = 0;
+
+				else
+					// inverse is simple inverse since sigma is diagonal
+					inverse[row][col] = 1 / Math.sqrt(l[row][col]);
+			}
+
+		// inline matrix multiply
+
+		double[][] av = new double[a.length][v[0].length];
+
+		for (int row = 0; row < av.length; row++) {
+			for (int col = 0; col < av[row].length; col++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < v.length; brow++)
+					sum += a[row][brow] * v[brow][col];
+
+				av[row][col] = sum;
+			}
+		}
+
+		// inline matrix multiply and tranpose
+
+		double[][] ut = new double[inverse[0].length][av.length];
+
+		for (int col = 0; col < ut.length; col++) {
+			for (int row = 0; row < ut[col].length; row++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < inverse.length; brow++)
+					sum += av[row][brow] * inverse[brow][col];
+
+				ut[col][row] = sum;
+			}
+		}
+
+		// 27 variables NOT counting any variable declared inside for loop
+
+		// inline matrix multiply
+
+		double[][] vsInv = new double[acol][acol];
+
+		// find dot products of every row and column combination
+
+		for (int row = 0; row < vsInv.length; row++) {
+			for (int col = 0; col < vsInv[row].length; col++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < acol; brow++)
+					sum += v[row][brow] * inverse[brow][col];
+
+				vsInv[row][col] = sum;
+			}
+		}
+
+		// inline matrix multiply
+
+		double[][] vsInvUt = new double[acol][a.length];
+
+		// find dot products of every row and column combination
+
+		for (int row = 0; row < vsInvUt.length; row++) {
+			for (int col = 0; col < vsInvUt[row].length; col++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < ut.length; brow++)
+					sum += vsInv[row][brow] * ut[brow][col];
+
+				vsInvUt[row][col] = sum;
+			}
+		}
+
+		// inline matrix multiply
+
+		double[][] x = new double[acol][1];
+
+		// find dot products of every row and column combination
+
+		for (int row = 0; row < x.length; row++) {
+			for (int col = 0; col < x[row].length; col++) {
+
+				// inline dot product
+
+				double sum = 0;
+
+				for (int brow = 0; brow < b.length; brow++)
+					sum += vsInvUt[row][brow] * b[brow][col];
+
+				x[row][col] = sum;
+			}
+		}
+
+		// inline matrix multiply on vector
+
+		double[][] e = new double[a.length][1];
+
+		// find dot products of every row and column combination
+
+		for (int row = 0; row < e.length; row++) {
+
+			// inline dot product
+
+			double sum = 0;
+
+			for (int brow = 0; brow < x.length; brow++)
+				sum += a[row][brow] * x[brow][0];
+
+			// change to matrix multiply to satisfy specifications
+
+			e[row][0] = sum - b[row][0];
+		}
+
+		return new double[][][] { x, e };
+	}
+
 }
--- MA 305/test/linearalgebra/ExtraTest.java
+++ MA 305/test/linearalgebra/ExtraTest.java
@@ -375,7 +375,7 @@ public class ExtraTest {
 	 * Test method for {@link Extra#overconstrainedCD(double[][], double[][])}.
 	 */
 	@Test
-	public void testOverconstraintedCD() {
+	public void testOverconstrainedCD() {
 		double[][] a;
 		double[][] b;
 		double[][][] actualXE;
@@ -587,7 +587,7 @@ public class ExtraTest {
 	 * Test method for {@link Extra#overconstrainedQR(double[][], double[][])}.
 	 */
 	@Test
-	public void testOverconstraintedQR() {
+	public void testOverconstrainedQR() {
 		double[][] a;
 		double[][] b;
 		double[][][] actualXE;
@@ -785,6 +785,218 @@ public class ExtraTest {
 		try {
 			// matrix b row width does not remain constant
 			testXE = Extra.overconstrainedQR(new double[][] {
+					new double[] { 0, 0 }, new double[] { 0, 0 },
+					new double[] { 0, 0 } }, new double[][] {
+					new double[] { 0 }, new double[] { 0 },
+					new double[] { 0, 0 } });
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+	}
+
+	/**
+	 * Test method for {@link Extra#overconstrainedSV(double[][], double[][])}.
+	 */
+	@Test
+	public void testOverconstrainedSV() {
+		double[][] a;
+		double[][] b;
+		double[][][] actualXE;
+		double[][] expectedX;
+		double[][] actualX;
+		double[][] expectedE;
+		double[][] actualE;
+		double[][] actualAX;
+		double[][][] testXE = null;
+
+		// first test on 3 by 2 and 3 by 1 arrays
+		a = new double[][] { new double[] { 4, -3 }, new double[] { -1, 1 },
+				new double[] { -4, -3 } };
+		b = new double[][] { new double[] { -1 }, new double[] { -3 },
+				new double[] { 4 } };
+		expectedX = new double[][] { new double[] { -0.535 },
+				new double[] { -0.660 } };
+		expectedE = new double[][] { new double[] { 0.839 },
+				new double[] { 2.875 }, new double[] { 0.120 } };
+		actualXE = Extra.overconstrainedSV(a, b);
+		actualX = actualXE[0];
+		actualE = actualXE[1];
+
+		assertEquals(expectedX.length, actualX.length);
+		for (int row = 0; row < expectedX.length; row++) {
+			assertEquals(expectedX[row].length, actualX[row].length);
+			for (int col = 0; col < expectedX[row].length; col++)
+				assertEquals(expectedX[row][col], actualX[row][col], DELTA);
+		}
+
+		assertEquals(expectedE.length, actualE.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualE[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualE[row][col], DELTA);
+		}
+
+		actualAX = Matrices.multiply(a, actualX);
+		assertEquals(expectedE.length, actualAX.length);
+		assertEquals(expectedE.length, b.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualAX[row].length);
+			assertEquals(expectedE[row].length, b[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualAX[row][col]
+						- b[row][col], DELTA);
+		}
+
+		// second test on 4 by 3 and 4 by 1 arrays
+		a = new double[][] { new double[] { -3, 4, 3 },
+				new double[] { -3, -2, -4 }, new double[] { 4, 2, -1 },
+				new double[] { 3, 1, -3 } };
+		b = new double[][] { new double[] { 2 }, new double[] { -3 },
+				new double[] { 1 }, new double[] { 1 } };
+		expectedX = new double[][] { new double[] { 0.239 },
+				new double[] { 0.473 }, new double[] { 0.266 } };
+		expectedE = new double[][] { new double[] { -0.030 },
+				new double[] { 0.273 }, new double[] { 0.637 },
+				new double[] { -0.607 } };
+		actualXE = Extra.overconstrainedSV(a, b);
+		actualX = actualXE[0];
+		actualE = actualXE[1];
+
+		assertEquals(expectedX.length, actualX.length);
+		for (int row = 0; row < expectedX.length; row++) {
+			assertEquals(expectedX[row].length, actualX[row].length);
+			for (int col = 0; col < expectedX[row].length; col++)
+				assertEquals(expectedX[row][col], actualX[row][col], DELTA);
+		}
+
+		assertEquals(expectedE.length, actualE.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualE[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualE[row][col], DELTA);
+		}
+
+		actualAX = Matrices.multiply(a, actualX);
+		assertEquals(expectedE.length, actualAX.length);
+		assertEquals(expectedE.length, b.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualAX[row].length);
+			assertEquals(expectedE[row].length, b[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualAX[row][col]
+						- b[row][col], DELTA);
+		}
+
+		// third test on 5 by 3 and 5 by 1 arrays
+		a = new double[][] { new double[] { 3, -1, -1 },
+				new double[] { -3, 2, -3 }, new double[] { 2, -4, -2 },
+				new double[] { -4, -3, -1 }, new double[] { 3, 1, -1 } };
+		b = new double[][] { new double[] { 3 }, new double[] { 3 },
+				new double[] { 1 }, new double[] { 1 }, new double[] { 4 } };
+		expectedX = new double[][] { new double[] { 0.307 },
+				new double[] { 0.232 }, new double[] { -1.318 } };
+		expectedE = new double[][] { new double[] { -0.994 },
+				new double[] { 0.497 }, new double[] { 1.319 },
+				new double[] { -1.606 }, new double[] { -1.530 } };
+		actualXE = Extra.overconstrainedSV(a, b);
+		actualX = actualXE[0];
+		actualE = actualXE[1];
+
+		assertEquals(expectedX.length, actualX.length);
+		for (int row = 0; row < expectedX.length; row++) {
+			assertEquals(expectedX[row].length, actualX[row].length);
+			for (int col = 0; col < expectedX[row].length; col++)
+				assertEquals(expectedX[row][col], actualX[row][col], DELTA);
+		}
+
+		assertEquals(expectedE.length, actualE.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualE[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualE[row][col], DELTA);
+		}
+
+		actualAX = Matrices.multiply(a, actualX);
+		assertEquals(expectedE.length, actualAX.length);
+		assertEquals(expectedE.length, b.length);
+		for (int row = 0; row < expectedE.length; row++) {
+			assertEquals(expectedE[row].length, actualAX[row].length);
+			assertEquals(expectedE[row].length, b[row].length);
+			for (int col = 0; col < expectedE[row].length; col++)
+				assertEquals(expectedE[row][col], actualAX[row][col]
+						- b[row][col], DELTA);
+		}
+
+		try {
+			// matrix a is null
+			testXE = Extra.overconstrainedSV(null, b);
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix b is null
+			testXE = Extra.overconstrainedSV(a, null);
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix a and b do not have equal number of rows
+			testXE = Extra.overconstrainedSV(
+					new double[][] { new double[] {} }, new double[][] {});
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix a does contain null column
+			testXE = Extra.overconstrainedSV(new double[][] { null },
+					new double[][] { null });
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix b does contain null column
+			testXE = Extra
+					.overconstrainedSV(new double[][] { new double[] {} },
+							new double[][] { null });
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix a must does not have more rows than columns
+			testXE = Extra.overconstrainedSV(new double[][] {
+					new double[] { 0, 0 }, new double[] { 0, 0 } },
+					new double[][] { new double[] { 0 }, new double[] { 0 } });
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix a row width does not remain constant
+			testXE = Extra.overconstrainedSV(new double[][] {
+					new double[] { 0, 0 }, new double[] { 0, 0 },
+					new double[] { 0, 0, 0 } },
+					new double[][] { new double[] { 0 }, new double[] { 0 },
+							new double[] { 0 } });
+			fail();
+		} catch (IllegalArgumentException ex) {
+			assertNull(testXE);
+		}
+
+		try {
+			// matrix b row width does not remain constant
+			testXE = Extra.overconstrainedSV(new double[][] {
 					new double[] { 0, 0 }, new double[] { 0, 0 },
 					new double[] { 0, 0 } }, new double[][] {
 					new double[] { 0 }, new double[] { 0 },
